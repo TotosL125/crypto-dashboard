671318f98c37aa38392fcc07c05c31048fe372de
TotosL125 <totosluke@gmail.com>
2023-05-10 15:10:35 +0200

    Add Dashboard component

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import Header from "../components/Header";
+
+const Dashboard: FC = () => {
+  return <Header title="Dashboard" />;
+};
+
+export default Dashboard;
