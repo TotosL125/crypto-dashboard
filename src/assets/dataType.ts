8bdf18adc3750651ba27e609c83450d89a28a975
TotosL125 <totosluke@gmail.com>
2023-05-11 19:49:44 +0200

    Update type definition for incoming data

--- src/assets/dataType.ts
+++ src/assets/dataType.ts
@@ -6,31 +6,13 @@ type dataObj = {
   current_price: number;
   market_cap: number;
   market_cap_rank: number;
-  fully_diluted_valuation: number;
-  total_volume: number;
   high_24h: number;
   low_24h: number;
   price_change_24h: number;
   price_change_percentage_24h: number;
-  circulating_supply: number;
-  total_supply: number;
-  max_supply: number;
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: string;
-  at1: number;
-  at1_change_percentage: number;
-  at1_date: string;
-  roi: null;
+  market_cap_change_24h: number;
+  market_cap_percentage_24h: number;
   last_updated: string;
 };
-
-// type dataObj = {
-//   id: string;
-//   symbol: string;
-//   name: string;
-//   current_price: number;
-//   price_change_percentage_24h: number;
-// };
 
 export default dataObj;
