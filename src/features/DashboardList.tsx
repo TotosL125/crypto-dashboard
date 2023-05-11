82e8c5a6e5b099eb436686ce7db0473d7718a801
TotosL125 <totosluke@gmail.com>
2023-05-11 11:38:47 +0200

    Add styling to DashboardList component
    - Center align contents on Dashboard page

--- src/features/DashboardList.tsx
+++ src/features/DashboardList.tsx
@@ -2,19 +2,24 @@ import React, { FC } from "react";
 
 import DashboardListItem from "./DashboardListItem";
 
+import styles from "./styles/DashboardList.module.css";
+
 import dataObj from "../assets/dataType";
 
 const DashboardList: FC<{ currencies: dataObj[] }> = (props) => {
   return (
-    <ul>
-      {props.currencies.map((currency) => (
-        <DashboardListItem
-          key={currency.id}
-          id={currency.id}
-          name={currency.name}
-        />
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <DashboardListItem id="id" name="Name" />
+      <ul>
+        {props.currencies.map((currency) => (
+          <DashboardListItem
+            key={currency.id}
+            id={currency.id}
+            name={currency.name}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
