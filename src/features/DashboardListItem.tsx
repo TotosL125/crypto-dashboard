088c6dc8cbdaaceece6ef3dec34a04af711e59ff
TotosL125 <totosluke@gmail.com>
2023-05-11 11:19:38 +0200

    Add styling to DashboardListItem component

--- src/features/DashboardListItem.tsx
+++ src/features/DashboardListItem.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 
+import styles from "./styles/DashboardListItem.module.css";
+
 const DashboardListItem: FC<{ id: string; name: string }> = (props) => {
   return (
     <Link to={`/${props.id}/detail`}>
-      <li>{props.name}</li>
+      <li className={styles.item}>{props.name}</li>
     </Link>
   );
 };
