cd940e9e21482505fde3bb9d53331728a9470806
TotosL125 <totosluke@gmail.com>
2023-05-12 10:44:59 +0200

    Add modal component
    This component is an overlay to the entire app.
    
    Used to show errors, etc.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,28 @@
+// library imports
+import React, { FC, Fragment, ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+const Backdrop: FC<{ onClick: () => void }> = (props) => {
+  return <div onClick={props.onClick} />;
+};
+
+const Overlay: FC<{ content: ReactNode }> = (props) => {
+  return <div>(props.content)</div>;
+};
+
+const Modal: FC<{ content: ReactNode; onClick: () => void }> = (props) => {
+  const backdropElement = document.getElementById(
+    "backdrop-root"
+  ) as HTMLElement;
+
+  const overlayElement = document.getElementById("overlay-root") as HTMLElement;
+
+  return (
+    <Fragment>
+      {createPortal(<Backdrop onClick={props.onClick} />, backdropElement)}
+      {createPortal(<Overlay content={props.content} />, overlayElement)}
+    </Fragment>
+  );
+};
+
+export default Modal;
