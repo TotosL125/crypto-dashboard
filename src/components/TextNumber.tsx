0d95f97a380ee547ded394609d7ba7bfe58568b2
TotosL125 <totosluke@gmail.com>
2023-05-13 18:27:47 +0200

    Add styling to TextNumber component

--- src/components/TextNumber.tsx
+++ src/components/TextNumber.tsx
@@ -1,9 +1,19 @@
+// library imports
 import React, { FC } from "react";
 
+// style imports
+import styles from "./styles/TextNumber.module.css";
+
 const TextNumber: FC<{ num: number }> = (props) => {
+  let style;
+  if (props.num > 0) {
+    style = styles.good;
+  } else {
+    style = styles.bad;
+  }
   return (
     <div>
-      <p>{props.num}%</p>
+      <p className={style}>{props.num}%</p>
     </div>
   );
 };
