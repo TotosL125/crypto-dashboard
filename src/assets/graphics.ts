e0272fcb29c5aff160c9b88adcd523149a5a3c1c
TotosL125 <totosluke@gmail.com>
2023-05-13 15:42:17 +0200

    Update svg imports
    - Names of SVG imports capitlized

--- src/assets/graphics.ts
+++ src/assets/graphics.ts
@@ -1,9 +1,9 @@
 // import SVGs
-import { ReactComponent as arrow } from "./SVGs/arrow-2-cw-svgrepo-com.svg";
-import { ReactComponent as checkmark } from "./SVGs/checkmark-square-svgrepo-com.svg";
-import { ReactComponent as cross } from "./SVGs/xmark-small-svgrepo-com.svg";
-import { ReactComponent as gear } from "./SVGs/gear-svgrepo-com.svg";
-import { ReactComponent as house } from "./SVGs/house-svgrepo-com.svg";
+import { ReactComponent as Arrow } from "./SVGs/arrow-2-cw-svgrepo-com.svg";
+import { ReactComponent as Checkmark } from "./SVGs/checkmark-square-svgrepo-com.svg";
+import { ReactComponent as Cross } from "./SVGs/xmark-small-svgrepo-com.svg";
+import { ReactComponent as Gear } from "./SVGs/gear-svgrepo-com.svg";
+import { ReactComponent as House } from "./SVGs/house-svgrepo-com.svg";
 
 // export all in single object
-export { arrow, checkmark, cross, gear, house };
+export { Arrow, Checkmark, Cross, Gear, House };
