c84df74d9aa21308fb9ab5961aa3a91dbb69ebb3
TotosL125 <totosluke@gmail.com>
2023-05-13 16:14:04 +0200

    Update ActionButton
    - Uses Icon component if icon prop is provided
    - Uses text if text prop is provided

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,15 +1,20 @@
+// library imports
 import React, { FC, SVGProps } from "react";
 
+// component imports
+import Icon from "./Icon";
+
 const ActionButton: FC<{
-  func: string | FC<SVGProps<SVGSVGElement>>;
+  icon?: FC<SVGProps<SVGSVGElement>>;
+  text?: string;
   onClick: () => void;
 }> = (props) => {
-  if (typeof props.func === "string") {
-    return <button onClick={props.onClick}>{props.func}</button>;
+  if (props.text) {
+    return <button onClick={props.onClick}>{props.text}</button>;
   } else {
     return (
       <button onClick={props.onClick}>
-        <props.func />
+        <Icon icon={props.icon!} />
       </button>
     );
   }
