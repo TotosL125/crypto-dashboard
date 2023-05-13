61e3b678db5ee8e6b352a6e19f8912f7e18d4446
TotosL125 <totosluke@gmail.com>
2023-05-13 16:58:42 +0200

    Add styling to Icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,8 +1,11 @@
 // library imports
 import React, { FC, SVGProps } from "react";
 
+// style imports
+import styles from "./styles/Icon.module.css";
+
 const Icon: FC<{ icon: FC<SVGProps<SVGSVGElement>> }> = (props) => {
-  return <props.icon />;
+  return <props.icon className={styles.icon} />;
 };
 
 export default Icon;
