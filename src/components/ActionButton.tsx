e687634587e8e90a6699f94707dede7688e9b4fa
TotosL125 <totosluke@gmail.com>
2023-05-13 17:18:09 +0200

    Add styling to ActionButton component

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -4,16 +4,23 @@ import React, { FC, SVGProps } from "react";
 // component imports
 import Icon from "./Icon";
 
+// style imports
+import styles from "./styles/ActionButton.module.css";
+
 const ActionButton: FC<{
   icon?: FC<SVGProps<SVGSVGElement>>;
   text?: string;
   onClick: () => void;
 }> = (props) => {
   if (props.text) {
-    return <button onClick={props.onClick}>{props.text}</button>;
+    return (
+      <button className={styles.button} onClick={props.onClick}>
+        {props.text}
+      </button>
+    );
   } else {
     return (
-      <button onClick={props.onClick}>
+      <button className={styles.button} onClick={props.onClick}>
         <Icon icon={props.icon!} />
       </button>
     );
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,7 +5,7 @@ import React, { FC, SVGProps } from "react";
 import styles from "./styles/Icon.module.css";
 
 const Icon: FC<{ icon: FC<SVGProps<SVGSVGElement>> }> = (props) => {
-  return <props.icon className={styles.icon} />;
+  return <props.icon className={styles.icon} height="4vh" />;
 };
 
 export default Icon;
