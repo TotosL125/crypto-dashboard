1dd2580665925f495c643e09dd6d4b8fa79b3536
TotosL125 <totosluke@gmail.com>
2023-05-13 21:21:29 +0200

    Update button and icon styling

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,7 +5,7 @@ import React, { FC, SVGProps } from "react";
 import styles from "./styles/Icon.module.css";
 
 const Icon: FC<{ icon: FC<SVGProps<SVGSVGElement>> }> = (props) => {
-  return <props.icon className={styles.icon} height="4vh" />;
+  return <props.icon className={styles.icon} transform="scale(0.2)" />;
 };
 
 export default Icon;
