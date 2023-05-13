1cddfc12e27306ed99c100a3f72689278279cbab
TotosL125 <totosluke@gmail.com>
2023-05-13 10:57:04 +0200

    Add graphics to assets folder
    Graphics used in buttons to show function

---
+++ src/assets/graphics.ts
@@ -0,0 +1,8 @@
+// import SVGs
+import arrow from "./SVGs/arrow-2-cw-svgrepo-com.svg";
+import checkmark from "./SVGs/checkmark-square-svgrepo-com.svg";
+import gear from "./SVGs/gear-svgrepo-com.svg";
+import house from "./SVGs/house-svgrepo-com.svg";
+
+// export all in single object
+export { arrow, checkmark, gear, house };
