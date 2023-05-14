a0136943f1614759fbd79c1b3eae5a76173803d4
TotosL125 <totosluke@gmail.com>
2023-05-14 18:16:52 +0200

    Add styling to error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,20 +1,22 @@
 // library imports
-import React, { FC, Fragment } from "react";
+import React, { FC } from "react";
 import { Link } from "react-router-dom";
 
 // component imports
 import Title from "../components/Title";
 import Text from "../components/Text";
 
+import styles from "./styles/Error.module.css";
+
 const ErrorPage: FC = () => {
   return (
-    <Fragment>
+    <div className={styles.layout}>
       <Title>404 Not Found</Title>
-      <Link to="/">
-        <p>Go back to safety!</p>
-      </Link>
       <Text>Congratulations! You have reached the end of the internet...</Text>
-    </Fragment>
+      <Link to="/">
+        <Text>Go back to safety!</Text>
+      </Link>
+    </div>
   );
 };
 
