0736305da0d94ba30ceb75b17d2dd8970875825a
TotosL125 <totosluke@gmail.com>
2023-05-14 09:36:18 +0200

    Add function to dynamically set Icon fill

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,8 +4,16 @@ import React, { FC, SVGProps } from "react";
 // style imports
 import styles from "./styles/Icon.module.css";
 
-const Icon: FC<{ icon: FC<SVGProps<SVGSVGElement>> }> = (props) => {
-  return <props.icon className={styles.icon} transform="scale(0.2)" />;
+const Icon: FC<{ icon: FC<SVGProps<SVGSVGElement>>; colour: string }> = (
+  props
+) => {
+  return (
+    <props.icon
+      className={styles.icon}
+      style={{ fill: props.colour }}
+      transform="scale(0.2)"
+    />
+  );
 };
 
 export default Icon;
