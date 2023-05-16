6958c8ce29b649ed0f759ffb4fc3836c9992846f
TotosL125 <totosluke@gmail.com>
2023-05-16 12:07:23 +0200

    Comment code in use-request.ts

--- src/hooks/use-request.ts
+++ src/hooks/use-request.ts
@@ -1,15 +1,18 @@
+// custom hook used to send http requests
 const useRequest = () => {
   const sendRequest = async (url: string) => {
-    const resp = await fetch(url);
+    const resp = await fetch(url); // send request and await response
 
+    // check if response from CoinGecko
     if (!resp.ok) {
-      throw new Error(`${resp.status}: ${resp.statusText}`);
+      throw new Error(`${resp.status}: ${resp.statusText}`); // "Error code: Error text"
     }
 
-    const data = await resp.json();
+    // if response is good
+    const data = await resp.json(); // get data from response
     return data;
   };
-  return sendRequest;
+  return sendRequest; // return the function
 };
 
 export default useRequest;
