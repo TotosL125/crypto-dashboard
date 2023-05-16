c4b4f64f1b44316c387ee4f06e577f29c17c06b4
TotosL125 <totosluke@gmail.com>
2023-05-16 10:00:57 +0200

    Make back button display conditionally
    - only displayed when on a detail page
    - changed icon to an arrow

--- src/assets/graphics.ts
+++ src/assets/graphics.ts
@@ -1,10 +1,10 @@
 // import SVG icons
-import { ReactComponent as Reload } from "./SVGs/rotate-circle-right-svgrepo-com.svg";
+import { ReactComponent as Back } from "./SVGs/chevron-left-svgrepo-com.svg";
 import { ReactComponent as Checkmark } from "./SVGs/check-svgrepo-com.svg";
 import { ReactComponent as Cross } from "./SVGs/cross-svgrepo-com.svg";
 import { ReactComponent as Gear } from "./SVGs/settings-svgrepo-com.svg";
-import { ReactComponent as House } from "./SVGs/home-1-svgrepo-com.svg";
+import { ReactComponent as Reload } from "./SVGs/rotate-circle-right-svgrepo-com.svg";
 import { ReactComponent as Warning } from "./SVGs/warning-circle-svgrepo-com.svg";
 
 // export all in single object
-export { Checkmark, Cross, Gear, House, Reload, Warning };
+export { Back, Checkmark, Cross, Gear, Reload, Warning };
--- src/layouts/RootLayoutHeader.tsx
+++ src/layouts/RootLayoutHeader.tsx
@@ -1,6 +1,6 @@
 // library imports
 import React, { FC, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 // component imports
 import HeaderButton from "./HeaderButton";
@@ -10,20 +10,23 @@ import styles from "./styles/RootLayoutHeader.module.css";
 
 // other imports
 import { CryptosContext } from "../stores/crypto-context";
-import { Gear, House, Reload } from "../assets/graphics";
+import { Back, Gear, Reload } from "../assets/graphics";
 
 const RootLayoutHeader: FC = (props) => {
   const { sendRequestHandler, settingsHandler } = useContext(CryptosContext);
+  const params = useParams();
 
   return (
     <div className={styles.header}>
-      <Link to="/">
-        <HeaderButton icon={House} onClick={() => {}} />
-      </Link>
       <div>
         <HeaderButton icon={Reload} onClick={sendRequestHandler} />
         <HeaderButton icon={Gear} onClick={settingsHandler} />
       </div>
+      {params.id && (
+        <Link to="/">
+          <HeaderButton icon={Back} onClick={() => {}} />
+        </Link>
+      )}
     </div>
   );
 };
